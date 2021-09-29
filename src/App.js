04671d77b9a34f57ce5c32eb2826404595409295
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-09-29 10:15:27 -0300

    UseReducer com Use context

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useReducer } from 'react';
+import './App.css';
+
+const globalState = {
+  title: 'O titulo que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+
+  return { ...state };
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+  return (
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-BR'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+    </div>
+  );
+}
+
+export default App;
